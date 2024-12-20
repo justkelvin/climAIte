d61ad2f8cce9e27159a083878840d38dfa4ffa5a
justkelvin <muia.muinde@s.karu.ac.ke>
2024-12-20 20:50:17 +0300

    feat: add search button to WeatherHomeScreen for location navigation

--- lib/features/weather/screens/weather_home_screen.dart
+++ lib/features/weather/screens/weather_home_screen.dart
@@ -2,6 +2,7 @@
 import 'package:climaite/features/weather/bloc/weather_bloc.dart';
 import 'package:climaite/features/weather/bloc/weather_event.dart';
 import 'package:climaite/features/weather/bloc/weather_state.dart';
+import 'package:climaite/features/weather/screens/location_search_screen.dart';
 import 'package:climaite/features/weather/widgets/current_weather_card.dart';
 import 'package:climaite/features/weather/widgets/daily_forecast_list.dart';
 import 'package:climaite/features/weather/widgets/hourly_forecast_list.dart';
@@ -49,6 +50,17 @@ class WeatherHomeScreen extends StatelessWidget {
                     centerTitle: true,
                   ),
                   actions: [
+                    IconButton(
+                      icon: const Icon(Icons.search),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LocationSearchScreen(),
+                          ),
+                        );
+                      },
+                    ),
                     IconButton(
                       icon: const Icon(Icons.settings),
                       onPressed: () {
